5fb381e092b21be7f4e72d5002979eb066850cc2
AbhishekS-2099 <abhishek.sankar.in@gmail.com>
2020-09-27 22:08:57 +0530

    Daily commit

---
+++ Search/countTriplets.cpp
@@ -0,0 +1,275 @@
+//https://www.hackerrank.com/challenges/triple-sum/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the triplets function below.
+long triplets(vector<int> a, vector<int> b, vector<int> c) {
+    
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    sort(c.begin(), c.end());
+ 
+    a.erase(unique(a.begin(),a.end()),a.end());
+    b.erase(unique(b.begin(),b.end()),b.end());
+    c.erase(unique(c.begin(),c.end()),c.end());
+    
+    int i = 0, j = 0, k = 0;
+    long sol = 0;
+    
+    while(i < b.size()){
+        while(a[j]<=b[i] && j<a.size())
+            j++;
+        while(c[k]<=b[i] && k<c.size())
+            k++;
+        sol+= (long)j*k;
+        i++;
+    }
+    return sol;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string lenaLenbLenc_temp;
+    getline(cin, lenaLenbLenc_temp);
+
+    vector<string> lenaLenbLenc = split_string(lenaLenbLenc_temp);
+
+    int lena = stoi(lenaLenbLenc[0]);
+
+    int lenb = stoi(lenaLenbLenc[1]);
+
+    int lenc = stoi(lenaLenbLenc[2]);
+
+    string arra_temp_temp;
+    getline(cin, arra_temp_temp);
+
+    vector<string> arra_temp = split_string(arra_temp_temp);
+
+    vector<int> arra(lena);
+
+    for (int i = 0; i < lena; i++) {
+        int arra_item = stoi(arra_temp[i]);
+
+        arra[i] = arra_item;
+    }
+
+    string arrb_temp_temp;
+    getline(cin, arrb_temp_temp);
+
+    vector<string> arrb_temp = split_string(arrb_temp_temp);
+
+    vector<int> arrb(lenb);
+
+    for (int i = 0; i < lenb; i++) {
+        int arrb_item = stoi(arrb_temp[i]);
+
+        arrb[i] = arrb_item;
+    }
+
+    string arrc_temp_temp;
+    getline(cin, arrc_temp_temp);
+
+    vector<string> arrc_temp = split_string(arrc_temp_temp);
+
+    vector<int> arrc(lenc);
+
+    for (int i = 0; i < lenc; i++) {
+        int arrc_item = stoi(arrc_temp[i]);
+
+        arrc[i] = arrc_item;
+    }
+
+    long ans = triplets(arra, arrb, arrc);
+
+    fout << ans << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
+
+
+
+/* Passes sample, but not main
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the triplets function below.
+long triplets(vector<int> a, vector<int> b, vector<int> c) {
+    map<int, int>map_a;
+    map<int, int>map_b;
+    map<int, int>map_c;
+    int size_a = a.size();
+    int size_b = b.size();
+    int size_c = c.size();
+    for(int i=0;i<size_a;i++){
+        if(map_a.find(a[i])==map_a.end()){
+            map_a.insert({a[i],1});
+        }else{
+            map_a[a[i]]++;
+        }
+    }
+    for(int i=0;i<size_b;i++){
+        if(map_b.find(b[i])==map_b.end()){
+            map_b.insert({b[i],1});
+        }else{
+            map_b[b[i]]++;
+        }
+    }
+    for(int i=0;i<size_c;i++){
+        if(map_c.find(c[i])==map_c.end()){
+            map_c.insert({c[i],1});
+        }else{
+            map_c[c[i]]++;
+        }
+    }
+    long count = 0;
+    for(auto i:map_b){
+        long count_a = 0;
+        long count_c = 0;
+        for(auto j: map_a){
+            if(i.first>=j.first){
+                cout<<"B: "<<i.first<<" "<<j.first<<endl;
+                count_a+=j.second;
+                }
+        }
+        for(auto j: map_c){
+            if(i.first>=j.first){
+                cout<<"C: "<<i.first<<" "<<j.first<<endl;
+                count_c+=j.second;
+                }
+        }
+        count+=count_a*count_c;
+    }
+    return count;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string lenaLenbLenc_temp;
+    getline(cin, lenaLenbLenc_temp);
+
+    vector<string> lenaLenbLenc = split_string(lenaLenbLenc_temp);
+
+    int lena = stoi(lenaLenbLenc[0]);
+
+    int lenb = stoi(lenaLenbLenc[1]);
+
+    int lenc = stoi(lenaLenbLenc[2]);
+
+    string arra_temp_temp;
+    getline(cin, arra_temp_temp);
+
+    vector<string> arra_temp = split_string(arra_temp_temp);
+
+    vector<int> arra(lena);
+
+    for (int i = 0; i < lena; i++) {
+        int arra_item = stoi(arra_temp[i]);
+
+        arra[i] = arra_item;
+    }
+
+    string arrb_temp_temp;
+    getline(cin, arrb_temp_temp);
+
+    vector<string> arrb_temp = split_string(arrb_temp_temp);
+
+    vector<int> arrb(lenb);
+
+    for (int i = 0; i < lenb; i++) {
+        int arrb_item = stoi(arrb_temp[i]);
+
+        arrb[i] = arrb_item;
+    }
+
+    string arrc_temp_temp;
+    getline(cin, arrc_temp_temp);
+
+    vector<string> arrc_temp = split_string(arrc_temp_temp);
+
+    vector<int> arrc(lenc);
+
+    for (int i = 0; i < lenc; i++) {
+        int arrc_item = stoi(arrc_temp[i]);
+
+        arrc[i] = arrc_item;
+    }
+
+    long ans = triplets(arra, arrb, arrc);
+
+    fout << ans << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
+*/
