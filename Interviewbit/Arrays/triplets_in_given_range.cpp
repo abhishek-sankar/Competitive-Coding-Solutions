c8bd29448149bd080e221c31fe523ba72bd68a6b
= <abhishek.sankar.in@gmail.com>
2020-10-14 23:37:03 +0530

    Added Header for Readme

---
+++ Interviewbit/Arrays/triplets_in_given_range.cpp
@@ -0,0 +1,48 @@
+/*
+https://www.interviewbit.com/problems/triplets-with-sum-between-given-range/
+Triplets with Sum between given range
+Bookmark Suggest Edit
+Given an array of real numbers greater than zero in form of strings.
+Find if there exists a triplet (a,b,c) such that 1 < a+b+c < 2 . 
+Return 1 for true or 0 for false.
+
+Example:
+
+Given [0.6, 0.7, 0.8, 1.2, 0.4] ,
+
+You should return 1
+
+as
+
+0.6+0.7+0.4=1.7
+
+1<1.7<2
+
+Hence, the output is 1.
+*/
+
+
+
+/*
+This solution gets a partial score
+int Solution::solve(vector<string> &A) {
+    vector<float>v;
+    for(int i=0;i<A.size();i++){
+        v.push_back(stof(A[i]));
+    }
+    // sort(v.begin(), v.end());
+    // for(int i=0;i<v.size();i++)cout<<v[i]<<endl;
+    for(int i=0;i<v.size()-2;i++){
+        if(v[i]>2) continue;
+        for(int j=i+1;j<v.size()-1;j++){
+            if(v[i]+v[j]>2)continue;
+            for(int k=j+1;k<v.size();k++){
+                if(v[i]+v[j]+v[k]>1 && v[i]+v[j]+v[k]<2)return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+
+*/
--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
+![Header Image](https://dev-to-uploads.s3.amazonaws.com/i/c4l7vr7g75jid85szu68.png)
 ### Hi there! 
 #### Its placement season, might as well make my github green with the questions I solve to stay in touch with the concepts. 
-- This is a collection of competitive coding questions mostly from [HackerRank](https://hackerrank.com) and their solutions in C++.
+- This is a collection of competitive coding questions from [HackerRank](https://hackerrank.com) and [InterviewBit](https://www.interviewbit.com/) and their solutions in C++.
 - Feel free to add in your solutions too!
