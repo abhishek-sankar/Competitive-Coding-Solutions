18b27c9b1f65aa7c89d499f077ad41003eeaf802
= <abhishek.sankar.in@gmail.com>
2021-04-02 16:38:29 +0530

    Test Driven Development, a beginning.

---
+++ cpp/leap/leap.cpp
@@ -0,0 +1,15 @@
+#include "leap.h"
+
+namespace leap {
+	int is_leap_year(int year){
+		if(year%4==0){
+			if(year%100==0 && year%400!=0){
+				return false;
+			}else{
+				return true;
+			}
+		}else{
+			return false;
+		}
+	}
+}  // namespace leap
