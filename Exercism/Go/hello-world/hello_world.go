533d72e53a54501aedd6afe47fc710462b0ef339
= <abhishek.sankar.in@gmail.com>
2021-04-04 00:07:06 +0530

    Added Go Track on Exercism

---
+++ Exercism/Go/hello-world/hello_world.go
@@ -0,0 +1,8 @@
+// Greeting the world, the programmer way!
+// https://golang.org/doc/effective_go.html#commentary
+package greeting
+
+// function that prints "Hello, World!" on the console
+func HelloWorld() string {
+	return "Hello, World!"
+}
