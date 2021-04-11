4c0d1ac9a751d7e7402c34fd442308163778a779
= <abhishek.sankar.in@gmail.com>
2021-04-11 18:06:05 +0530

    Completed Exercise: Raindrops // C++

--- Exercism/cpp/raindrops/raindrops.cpp
+++ Exercism/cpp/raindrops/raindrops.cpp
@@ -5,25 +5,12 @@ namespace raindrops
 	std::string convert(int number)
 	{
 		std::string result;
-		if (number % 3 != 0 && number % 5 != 0 && number % 7 != 0)
-		{
-			result = std::to_string(number);
-		}
-		else
-		{
-			if (number % 3 == 0)
-			{
-				result.append("Pling");
-			}
-			if (number % 5 == 0)
-			{
-				result.append("Plang");
-			}
-			if (number % 7 == 0)
-			{
-				result.append("Plong");
+		std::array<std::pair<int, std::string>, 3> number_sounds = {{{3, "Pling"}, {5, "Plang"}, {7, "Plong"}}};
+		for(auto number_sound_pair : number_sounds){
+			if(number % number_sound_pair.first == 0){
+				result.append(number_sound_pair.second);
 			}
 		}
-		return result;
+		return result.empty()?std::to_string(number):result;
 	}
 } // namespace raindrops
--- Exercism/cpp/raindrops/raindrops.h
+++ Exercism/cpp/raindrops/raindrops.h
@@ -1,7 +1,7 @@
 #if !defined(RAINDROPS_H)
 #define RAINDROPS_H
 #include <string>
-
+#include <array>
 namespace raindrops {
 	std::string convert(int number);
 }  // namespace raindrops
