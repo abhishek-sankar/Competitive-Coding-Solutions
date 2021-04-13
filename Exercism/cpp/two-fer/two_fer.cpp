cf206241f1f1ea03639707bdd6f3bd7c71ace58e
= <abhishek.sankar.in@gmail.com>
2021-04-13 22:37:45 +0530

    Completed Exercise: two-fer // C++

---
+++ Exercism/cpp/two-fer/two_fer.cpp
@@ -0,0 +1,11 @@
+#include "two_fer.h"
+
+namespace two_fer{
+	std::string two_fer(std::string name){
+		return "One for " + name + ", one for me.";
+	}
+	std::string two_fer(){
+		return "One for you, one for me.";
+	}
+} // namespace two_fer
+
